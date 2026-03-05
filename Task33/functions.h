ac703fe147d2c87d35622d493f6ac0aed673d3e2
fochuk-maksym <fochuk.maksym@chnu.edu.ua>
2026-03-05 23:05:28 +0200

    3 завдання

---
+++ Task33/functions.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+
+void inputArrayToTextFile();      // 3.1
+void randomArrayToBinaryFile();   // 3.2
+void solveTask();                 // 3.3
+void readVectorFromFile();        // 3.4
